117f330bc1eea847175d897c0c4d2b34dc3d3722
unknown <fredrickbolutife@gmail.com>
2024-01-25 10:32:50 +0100

    backend first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+const app = express();
+
+
+// Middleware to parse JSON in the request body
+app.use(express.json());
+
+const port = process.env.PORT;
+
+// Get Request
+app.get('/', (req, res) => {
+res.send("Hello Senior Devs!😂😁😀😁");
+})
+
+// Post Request
+app.post('/post', (req, res) => {
+        const {userName, password} = req.body;
+    res.send(`Hello ${userName} This is your password${password}`);
+})
+// Put Request
+app.put('/put', (req, res) => {
+    res.send('This is a put request');
+});
+
+
+app.listen(port, ()=> {
+    console.log(`Server is listening on http://localhost:${port}`);
+})
