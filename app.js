f82718b3a316cf67083413ef0c14471e35683339
unknown <fredrickbolutife@gmail.com>
2024-02-05 14:45:43 +0100

    added mongoose

--- app.js
+++ app.js
@@ -1,27 +1,57 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 dotenv.config();
 
 
-const app = express();
 
+mongoose.connect("mongodb://localhost:27017/blog")
+
+const app = express();
+app.use(morgan('dev'));
 
 // Middleware to parse JSON in the request body
 app.use(express.json());
 
-const port = process.env.PORT;
+
+const port = process.env.PORT || 3000;
+
+
+// Blog Schema
+
+const blogSchema = new mongoose.Schema({
+
+author: String,
+content: String,
+title: String
+
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
+
 
 // Get Request
 app.get('/', (req, res) => {
-res.send("Hello Senior Devs!😂😁😀😁");
-})
+res.send('Home Page')
+});
 
 // Post Request
-app.post('/post', (req, res) => {
-        const {userName, password} = req.body;
-    res.send(`Hello ${userName} This is your password${password}`);
+app.post('/post', async (req, res) => {
+        const {author, content, title} = req.body;
+
+const newBlog = new Blog({
+    author,
+    content,
+    title
 })
+
+await newBlog.save();
+
+res.send("Blog Created Successfully")
+})
+
+
 // Put Request
 app.put('/put', (req, res) => {
     res.send('This is a put request oooooooo');
